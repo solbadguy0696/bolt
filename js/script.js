f37b29cdac6e1046a05f4f8899e1a976089f4afa
solbadguy0696 <toymoon10@gmail.com>
2017-09-28 12:57:36 +0900

    javascriptを短い記述に変更,

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
-function navToggle() {
-  /* togglebtnはid#nav-toggle */
-  var toggleBtn= document.getElementById('nav-toggle');
-  var navView= document.getElementById('nav-list');
-  /* クラス名の取得 */
-  var toggleBtnClass= toggleBtn.getAttribute('class');
+var nav = document.getElementById('nav-toggle');
+var menu = document.getElementById('nav-list');
 
-  /* togglebtn(nav-toggle)がもしもこのクラス名であったら */
-  if(toggleBtnClass== 'fa fa-reorder header-bar__nav__contact--close btn-close') {
-    /* クラス名を外す */
-    toggleBtn.classList.remove('btn-close');
-    navView.classList.remove('list-close');
-
-    /* クラス名を付与する */
-    toggleBtn.classList.add('btn-open');
-    navView.classList.add('list-open');
-
-  /* もしもそうではなかったら */
-  }else {
-    /* クラス名を外す */
-    toggleBtn.classList.remove('btn-open');
-    navView.classList.remove('list-open');
-
-    /* クラス名を付与する */
-    toggleBtn.classList.add('btn-close');
-    navView.classList.add('list-close');
-  }
-}
-
-document.getElementById('nav-toggle').onclick = navToggle;
+nav.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+});
